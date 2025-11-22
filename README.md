55c2a4c545ddf678e6942792d2f7d8bd7455db59
mannpatel <tezla2997@gmail.com>
2025-11-22 01:17:43 -0700

    fixed and added functions

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/THJ5OpUp)
 # CMPUT 291 Project 2 - Fall 2025  
 Group member names and ccids (2-3 members)  
-  ccid1, name1  
+  mannjayp, Mann Patel 
   ccid2, name2  
   ccid3, name2  
 
--- phase2_query.py
+++ phase2_query.py
@@ -23,10 +23,10 @@ def main_menu(collection):
         article_count_difference(collection)
 
     if choice =="3":
-        top_5_news_sources(db)
+        top_5_news_sources(collection)
 
     if choice =="4":
-        most_recent_articles(db)
+        most_recent_articles(collection)
 
     if choice =="5":
         break
@@ -59,21 +59,34 @@ def most_common_words(collection):
             { "$match": { "word": {"$regex": "^[a-z]+$"}}},
             { "$group": {"_id":"$word","count": {"$sum": 1}}},
             { "$sort": { "count": -1 }},
-            { "$limit": 5 }                  
+            #{ "$limit": 5 }                  
             ]
             
         
         words_list = list(collection.aggregate(common_words))
+
         print("Most Common Words by Media Type: \n")
         
 
         if not words_list:
             print("No words found.\n")
-        else:
-            for i, doc in enumerate(words_list, start=1):
-                print(f"{i}. {doc['_id']} — {doc['count']}")
 
-        return most_common_words(collection)
+        if len(words_list) < 5:
+            last_element = words_list[-1]['count']
+        
+        else:
+            last_element = words_list[4]['count']
+
+        top_5_common_words = []
+
+        for doc in words_list:
+            if doc['count'] >= last_element:
+                top_5_common_words.append(doc)
+
+        for i, doc in enumerate(top_5_common_words, start=1):
+            print(f"{i}. {doc['_id']} — {doc['count']}")
+
+        
             
     
 def article_count_difference(collection):
@@ -96,56 +109,113 @@ def article_count_difference(collection):
         ]
 
         articles = list(collection.aggregate(article_count))
+
+        if not articles:
+            print("No articles were published on this day.\n")
         for doc in articles:
-            print(f"{doc['_id']} - {doc['count']}")
+            print(f"{doc['_id']} - {doc['count']}\n")
 
-       
+        news_count = 0
+        blog_count = 0
 
+        for doc in articles:
+            if doc['_id'] == "news":
+                news_count = doc['count']
+            
+            if doc['_id'] == "blog":
+                blog_count = doc['count']
         
+        if news_count > blog_count:
+            difference_1 = news_count - blog_count
+            print("The number of News articled are greater than blog articles by", difference_1)
+            
         
+        elif blog_count > news_count:
+            difference_2 = blog_count - news_count
+            print("The number of Blog articles are greater than news articles by", difference_2)
+            
 
-
-
-
-
-
-
-
-
-        return article_count_difference(collection)
+        else:
+            print("They both have same number of articles!!!")
 
 def most_recent_articles(collection):
+            while True:
+                source_name = input("Please enter a source or press 'q' to return to the main menu: ").strip().lower()
 
+                if source_name == "q":
+                    print("Returning to the Main Menu...\n")
+                    return
+                
+                recent_articles = [
+                    {"$match": {"source": {"$regex": f"^{source_name}$", "$options": "i"}}},
+                    
+                    #{"$limit": 5 },
+                    {"$project": {"title": 1,"published":1, "date": {"$substr": ["$published", 0,10 ]}}},
+                    {"$sort": { "published": -1 }}
+                ]
+                articles = list(collection.aggregate(recent_articles))
 
+                print("Top 5 recent article from", source_name)
 
+                if not articles:
+                    print("There are no recent articles associated with this source.\n")
+                    continue
 
+                if len(articles) < 5:
+                    last_element = articles[-1]['published']
+                
+                else:
+                    last_element = articles[4]['published']
 
+                top_5_recent = []
 
+                for doc in articles:
+                    if doc['published'] >= last_element:
+                        top_5_recent.append(doc)
 
-
-
-    return most_recent_articles(collection)
+                 
+                for doc in top_5_recent:
+                    print(f"{doc['title']} - {doc['date']}\n")
 
 def top_5_news_sources(collection):
+    while True:
 
+        sources = [
+            {"$match": {"media-type": {"$regex": "^news$", "$options": "i"}, "published": {"$regex": r"^2015"}}},
+            {"$group": {"_id": "$source", "count": {"$sum": 1}}},
+            { "$sort": { "count": -1 }},
+            #{ "$limit": 5 }
+        ]
+        
+        news_sources = list(collection.aggregate(sources))
 
+        print("\n")
+        print("Top 5 News Sources:\n")
 
+        if not news_sources:
+            print("No sources were published by news articles in 2015.\n")
 
+        if len(news_sources) < 5:
+            last_element = news_sources[-1]['count']
 
+        else:
+            last_element = news_sources[4]['count']
+    
+        top_5_sources = []
 
+        for doc in news_sources:
+            if doc['count'] >= last_element:
+                top_5_sources.append(doc)
 
+        for doc in top_5_sources:
+            print(f"{doc['_id']} - {doc['count']}\n")
 
+        return
 
-
-
-
-
-    return top_5_news_sources(collection)
-  
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("❗ Usage: python main.py <database_file>")
+        print("❗ Usage: python phase2_query.py {port_number}")
         sys.exit()
 
     port_number = sys.argv[1]
